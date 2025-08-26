2772dc823e379097b802eb9f8a28873af81431fd
Temenu Michael <timmytems680@gmail.com>
2025-08-26 17:53:24 +0100

    commit

--- src/Components/Layout/UserLayout.tsx
+++ src/Components/Layout/UserLayout.tsx
@@ -2,7 +2,6 @@ import Header from '../Common/Header'
 import NavBar from '../Common/NavBar'
 import Footer from '../Common/Footer'
 import { Outlet } from 'react-router-dom'
-import Home from '../../Pages/Home'
 
 const UserLayout = () => {
   return (
